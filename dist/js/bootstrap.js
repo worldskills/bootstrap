ee0546cb2bc544a938c378a1cfc3f4ced4ca088e
Fabian Vogler <fabian@equivalence.ch>
2016-01-25 14:16:19 +0100

    Remove jqueryVersionCheck

--- dist/js/bootstrap.js
+++ dist/js/bootstrap.js
@@ -1,20 +1,12 @@
 /*!
  * Bootstrap v3.3.6 (http://getbootstrap.com)
- * Copyright 2011-2015 Twitter, Inc.
+ * Copyright 2011-2016 Twitter, Inc.
  * Licensed under the MIT license
  */
 
 if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
 }
-
-+function ($) {
-  'use strict';
-  var version = $.fn.jquery.split(' ')[0].split('.')
-  if ((version[0] < 2 && version[1] < 9) || (version[0] == 1 && version[1] == 9 && version[2] < 1) || (version[0] > 2)) {
-    throw new Error('Bootstrap\'s JavaScript requires jQuery version 1.9.1 or higher, but lower than version 3')
-  }
-}(jQuery);
 
 /* ========================================================================
  * Bootstrap: transition.js v3.3.6
